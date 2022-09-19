c77f1bc27cbc9689ebb421864f2e5c3b135fc5f4
AminIsmail991 <98789790+AminIsmail991@users.noreply.github.com>
2022-09-19 15:16:53 +0800

    Add files via upload

---
+++ activity/javascripting/function-arguments.js
@@ -0,0 +1,9 @@
+function math(first, second, third) {
+    return (third * second) + first
+}
+console.log(math(53, 61, 67));
+
+// function math (a, b, c) {
+//     return (b * c) + a
+//   }
+//   console.log(math(53, 61, 67))
---
+++ activity/javascripting/rounding-numbers.js
@@ -0,0 +1,7 @@
+const roundUp = Math.round(1.5)
+const rounded = roundUp
+console.log(rounded);
+
+// const roundUp = 1.5
+// const rounded = Math.round(roundUp)
+// console.log(rounded)
